58c6e07d05bea5fe15a7d62bde3150014762dcc8
RyanHailey21 <ryanhaileygolf@gmail.com>
2025-05-14 13:08:45 -0500

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import mdx from '@astrojs/mdx'
 
 
 export default defineConfig({
