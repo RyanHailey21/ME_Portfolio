69dabe429a6470a765e68757445d8403ea946d5a
RyanHailey21 <ryanhaileygolf@gmail.com>
2025-05-14 09:50:33 -0500

    Astro congfig update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import mdx from '@astrojs/mdx';
 
+// https://astro.build/config
 export default defineConfig({
+  // Replace 'ryanh' with your GitHub username if different
+  // Replace 'project' with your GitHub repository name if different
+  site: 'https://ryanhailey21.github.io', 
+  base: '/ME_Portofolio', 
   integrations: [tailwind(), mdx()]
 });
