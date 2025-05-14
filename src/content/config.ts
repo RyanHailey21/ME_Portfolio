ba10e0e3b00154322f5f0dac68d6990ff9bf045d
RyanHailey21 <ryanhaileygolf@gmail.com>
2025-05-14 11:53:21 -0500

    Handle media more efficeinty for build

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,10 +2,10 @@ import { defineCollection, z }  from "astro:content";
 
 const projectCollection = defineCollection({
     type:'content',
-    schema: z.object({
+    schema: ({ image }) => z.object({
         title: z.string(),
         description: z.string(),
-        image: z.string(),
+        image: image(),
         tags: z.array(z.string()),
         technologies: z.array(z.string()),
         githubUrl: z.string().optional(),
